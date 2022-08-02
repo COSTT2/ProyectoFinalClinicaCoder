6e46df4f6486856a81b738d39ee33b8bf5670311
Coste Tiago <coste.tiago@gmail.com>
2022-08-01 23:55:03 -0300

    CRUD y comienzo de login

--- proyectoFinal/academia/models.py
+++ proyectoFinal/academia/models.py
@@ -1,3 +1,18 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+class Cursos(models.Model):
+    titulo = models.CharField(max_length=40)
+    descripcion = models.CharField(max_length=100)
+    contenido = RichTextField()
+    imagen = models.ImageField(upload_to="articulos", null=True, blank=True)
+    duracion = models.Charfield(max_lenght=40)
+    
+class profesores(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    especialidad = models.CharField(max_length=40)
+    mail = models.EmailField(max_length=100)
+    educacion = models.CharField(max_length=40)
+    imagen = models.ImageField(upload_to="articulos", null=True, blank=True)
